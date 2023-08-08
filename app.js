8fd6f73f214010aedf070061f4fb86b713747d78
jose luis condori <jose.l.condori@gmail.com>
2023-08-08 10:41:37 -0300

    mensaje

---
+++ app.js
@@ -0,0 +1,24 @@
+const evenOrOdd  = require('num-is-even-or-odd');
+
+
+//metodos
+/*
+variabke.nombre en ingles()
+
+
+ */
+
+/*funcion 
+evenorodd(25)
+*/
+console.log(evenOrOdd (25))
+console.log(evenOrOdd (90))
+console.log(' este numero es del tipo',evenOrOdd(87))
+console.log('buenas noches como estas')
+// console.log('hola mundo')
+
+// function saludo(nombre){
+//     console.log(`hola buenos dias ${nombre}`)
+// }
+
+// saludo('manuel')
